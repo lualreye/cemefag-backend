4ed068d940eac2747eed4bca277f9279fa389e48
lualreye <lualreye@gmail.com>
2022-03-30 17:08:50 -0500

    feat: handling jwt according to the database type

--- utils/handleJwt.js
+++ utils/handleJwt.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET
+const getProperties = require("../utils/handlePropertiesEngine")
+const propertiesKey = getProperties()
 
 // PASS USER OBJECT
 // @params {*} user
 const tokenSign = async (user) => {
   const sign = jwt.sign(
     {
-      _id: user._id,
+      [propertiesKey.id]: user[propertiesKey.id],
       role: user.role
     },
     JWT_SECRET
