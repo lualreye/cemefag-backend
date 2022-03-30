e83d111f1472d919e5b8cc88e99233ca5138bbe6
lualreye <lualreye@gmail.com>
2022-03-30 17:03:55 -0500

    feat: proccesing path according to the database type

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
+const ENGINE_DB = process.env.ENGINE_DB
+
+const pathModels = (ENGINE_DB === "nosql") ? './nosel' : "./mysql"
+
 const models = {
-  patientModel: require("./nosql/patient"),
-  userModel: require("./nosql/users"),
-  consultationModel: require("./nosql/consultation")
-}
+  patientModel: require(`${pathModels}/patient`),
+  userModel: require(`${pathModels}/users`),
+  consultationModel: require(`${pathModels}/consultation`),
+};
 
 module.exports = models
