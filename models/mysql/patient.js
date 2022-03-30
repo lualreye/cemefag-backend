b2aa707120779c51c944043a927a69d20de32d31
lualreye <lualreye@gmail.com>
2022-03-30 13:49:46 -0500

    add: mysql models added

---
+++ models/mysql/patient.js
@@ -0,0 +1,42 @@
+const { sequelize } = require("../../config/mysql");
+const { Datatypes } = require("sequelize");
+
+const Patient = sequelize.define(
+  "patient",
+  {
+    cedula: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    birthday: {
+      type: Datatypes.DATE,
+      allowNull: false,
+    },
+    email: {
+      type: Datatypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    phone: {
+      type: Datatypes.STRING,
+      allowNull: false,
+    },
+    product: {
+      type: Datatypes.NUMBER,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = { Patient };
