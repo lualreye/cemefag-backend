1f9e9587349e115e0d4c6b337a944c2d8b88dc59
lualreye <lualreye@gmail.com>
2022-03-31 16:58:08 -0500

    fix: validations in patient

--- validators/patient.js
+++ validators/patient.js
@@ -2,24 +2,23 @@ const { check } = require("express-validator");
 const validateResults = require("../utils/handleValidator");
 
 const validatorCreatePatient = [
-  check("name").exists().notEmpty(),
-  check("lastname").exists().notEmpty(),
-  check("birthday").exists().notEmpty(),
-  check("cedula").exists().notEmpty(),
-  check("email").exists().notEmpty(),
-  check("phone").exists().notEmpty(),
-  check("product").exists().notEmpty(),
-  check("role").exists().notEmpty(),
+  check("pc_nombres").exists().notEmpty(),
+  check("pc_apellidos").exists().notEmpty(),
+  check("pr_fecha_nacim").exists().notEmpty(),
+  check("pc_cedula").exists().notEmpty(),
+  check("pc_mail").exists().notEmpty(),
+  check("pc_celular").exists().notEmpty(),
+  check("pr_producto").exists().notEmpty(),
   (req, res, next) => {
     validateResults(req, res, next);
   },
 ];
 
 const validatorGetPatient = [
-  check("id").exists().notEmpty(),
+  check("pc_id").exists().notEmpty(),
   (req, res, next) => {
-    validateResults(req, res, next)
-  }
+    validateResults(req, res, next);
+  },
 ];
 
 module.exports = { validatorCreatePatient, validatorGetPatient };
