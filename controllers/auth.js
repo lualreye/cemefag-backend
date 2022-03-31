edd3240dbf40257ba848165e91cdb5dff3198418
lualreye <lualreye@gmail.com>
2022-03-31 02:08:48 -0500

    feat: login correctly

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,17 @@ const { matchedData } = require("express-validator");
 const { tokenSign } = require("../utils/handleJwt");
 const { userModel } = require("../models/index");
 const { handleHttpError } = require("../utils/customHttpErros");
-const { compare } = require("bcryptjs");
-const md5 = require("md5")
+const md5 = require("md5");
 
 const registerController = async (req, res) => {
   try {
     req = matchedData(req);
     const hashPassword = await md5(req.us_clave);
     const body = { ...req, us_clave: hashPassword };
-    console.log(body)
+    console.log(body);
     const userData = await userModel.create(body);
-    console.log("No estamos llegando auqi")
-    console.log(userData)
+    console.log("No estamos llegando auqi");
+    console.log(userData);
     userData.set("us_clave", undefined, { strict: false });
 
     const data = {
@@ -30,16 +29,16 @@ const registerController = async (req, res) => {
 const loginController = async (req, res) => {
   try {
     req = matchedData(req);
-    console.log(req)
-    const user = await userModel
-      .findOne({ us_nombre: req.us_usuario })
+    const user = await userModel.findOne({
+      where: { us_nombre: req.us_nombre },
+    });
     if (!user) {
       handleHttpError(res, "USER_DOES_NOT_EXIST");
       return;
     }
 
     const hashPassword = user.get("us_clave");
-    const check = md5(req.us_clave) ===  hashPassword;
+    const check = md5(req.us_clave) === hashPassword;
     if (!check) {
       handleHttpError(res, "PASSWORD_INVALID", 401);
       return;
