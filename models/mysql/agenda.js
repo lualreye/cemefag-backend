0d92203050aa9ee7a9afbfa2535e766da860a4ee
lualreye <lualreye@gmail.com>
2022-04-06 03:47:37 -0500

    fix: getting data related to patient

--- models/mysql/agenda.js
+++ models/mysql/agenda.js
@@ -1,33 +1,65 @@
 const { sequelize } = require("../../config/mysql");
 const { DataTypes } = require("sequelize");
+const Patient = require("./patient");
 
 const Agenda = sequelize.define(
   "agenda",
   {
     ag_id: {
-      type: DataTypes.NUMBER,
+      type: DataTypes.INTEGER,
       primaryKey: true,
+    },
+    pc_id: {
+      type: DataTypes.INTEGER,
+    },
+    pr_id: {
+      type: DataTypes.INTEGER,
+    },
+    es_id: {
+      type: DataTypes.INTEGER,
+    },
+    me_id: {
+      type: DataTypes.INTEGER,
+    },
+    us_id: {
+      type: DataTypes.INTEGER,
     },
     ag_fecha_reg: {
       type: DataTypes.DATE,
     },
-    as_fecha_age: {
-      type: DataTypes.DATE
+    ag_fecha_age: {
+      type: DataTypes.DATE,
+    },
+    tp_id: {
+      type: DataTypes.INTEGER,
     },
     ag_cobertura: {
-      type: DataTypes.NUMBER
+      type: DataTypes.INTEGER,
     },
     ag_valor: {
-      type: DataTypes.NUMBER
+      type: DataTypes.INTEGER,
     },
     ag_atendido: {
-      type: DataTypes.BOOLEAN
-    }
+      type: DataTypes.BOOLEAN,
+    },
   },
   {
     timestamp: false,
     freezeTableName: true,
+    createdAt: false,
+    updatedAt: false,
   }
 );
+
+// IMPLEMENTING PERSONALIZE MODEL
+
+Agenda.findOneAgenda = function (pc_id) {
+  Agenda.belongsTo(Patient, {
+    foreignKey: "pc_id",
+    as: "paciente",
+  });
+
+  return Agenda.findOne({ where: { pc_id }, include: "paciente" });
+};
 
 module.exports = Agenda;
