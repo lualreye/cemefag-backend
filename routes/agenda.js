0860fc26ef14cd04b2a5f0ba08879c52428fb3fa
lualreye <lualreye@gmail.com>
2022-04-06 03:44:03 -0500

    fix: agenda route

--- routes/agenda.js
+++ routes/agenda.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+const { getAgenda } = require("../controllers/agenda")
+
 const { authMiddleware } = require("../middlewares/session");
 
 // TODO http://localhost:3000/api/agenda/pc_id
 
 
 // GETTING PATIENT AGENDA
-router.get("/:id", authMiddleware, getAgenda);
+router.get("/:pc_id", authMiddleware, getAgenda);
 
 // CREATING PATIENT AGENDA
-router.post("/", authMiddleware, createAgenda);
+// router.post("/", authMiddleware, createAgenda);
 
 // UPDATING PATIENT AGENDA
-router.put("/:id", authMiddleware, updateAgenda);
+// router.put("/:id", authMiddleware, updateAgenda);
 
 module.exports = router;
