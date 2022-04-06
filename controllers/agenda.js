165627561cf9f35818f2652b98029d0c70fa77b6
lualreye <lualreye@gmail.com>
2022-04-06 03:46:26 -0500

    feat: getting data

--- controllers/agenda.js
+++ controllers/agenda.js
@@ -1,2 +1,18 @@
-const matchedData = require("express-validator")
-const { agendaModel } = require("")
+const { agendaModel } = require("../models/index");
+const { handleHttpError } = require("../utils/customHttpErros");
+
+// GETTING PATIENT DETAIL
+
+// params {*} req
+// params {*} res
+const getAgenda = async (req, res) => {
+  try {
+    const pc_id = Number(req.params.pc_id)
+    const data = await agendaModel.findOneAgenda(pc_id);
+    res.send({ data });
+  } catch (e) {
+    handleHttpError(res, "CANNOT_GET_AGENDA_DETAIL", e);
+  }
+};
+
+module.exports = { getAgenda };
