32dc1fcee0c7c2daf694ab7952ab61ffbdc99ff3
lualreye <lualreye@gmail.com>
2022-04-06 03:44:44 -0500

    add: agenda added

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
-const ENGINE_DB = process.env.ENGINE_DB
+const ENGINE_DB = process.env.ENGINE_DB;
 
-const pathModels = (ENGINE_DB === "nosql") ? './nosql' : "./mysql"
+const pathModels = ENGINE_DB === "nosql" ? "./nosql" : "./mysql";
 
 const models = {
   patientModel: require(`${pathModels}/patient`),
   userModel: require(`${pathModels}/users`),
-  consultationModel: require(`${pathModels}/consultation`),
+  agendaModel: require(`${pathModels}/agenda`),
 };
 
-module.exports = models
+module.exports = models;
