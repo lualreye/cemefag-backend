0708282981d278ac6a32b58ac2e4bd37d2135378
lualreye <lualreye@gmail.com>
2022-04-09 03:19:27 -0500

    add: getting relation speciality and doctors route

--- routes/speciality.js
+++ routes/speciality.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getSpecialities } = require("../controllers/speciality");
+const {
+  getSpecialities,
+  getSpecialityRelation,
+} = require("../controllers/speciality");
 
 const { authMiddleware } = require("../middlewares/session");
 
@@ -9,5 +12,8 @@ const { authMiddleware } = require("../middlewares/session");
 
 // TODO: getting specialities list
 router.get("/", authMiddleware, getSpecialities);
+
+// TODO: getting all doctors related to the speciality params
+router.get("/:es_id", authMiddleware, getSpecialityRelation);
 
 module.exports = router;
